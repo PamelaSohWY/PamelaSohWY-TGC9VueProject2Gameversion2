8438f3445f2bf6e3fbdac4666e0c062023080a4c
PamelaSohWY <pamelasohwy@gmail.com>
2020-12-18 09:25:48 +0000

    Recreated readme and created basic vue page

---
+++ game/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+      <Basic></Basic>
+  </div>
+</template>
+
+<script>
+
+import Basic from "./components/Basic"
+
+export default {
+  name: 'App',
+  components: {
+      Basic
+  }
+}
+</script>
+
+<style>
